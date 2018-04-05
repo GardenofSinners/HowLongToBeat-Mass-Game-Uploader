c33779df4edd6cdef646933586b98e6355ce4f99
GardenofSinners <connectionsincredible@gmail.com>
2018-04-05 14:34:57 +0100

    Initial Commit

---
+++ HowLongToBeat Game/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace HowLongToBeat_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogueButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(111, 15);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(196, 20);
+            this.username.TabIndex = 0;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(111, 38);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(196, 20);
+            this.password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.label2.Location = new System.Drawing.Point(19, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(295, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(1, 36);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(784, 615);
+            this.webBrowser1.TabIndex = 5;
+            this.webBrowser1.Url = new System.Uri("https://howlongtobeat.com/login.php", System.UriKind.Absolute);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialogueButton
+            // 
+            this.openFileDialogueButton.Location = new System.Drawing.Point(1, 4);
+            this.openFileDialogueButton.Name = "openFileDialogueButton";
+            this.openFileDialogueButton.Size = new System.Drawing.Size(146, 31);
+            this.openFileDialogueButton.TabIndex = 6;
+            this.openFileDialogueButton.Text = "Browse for Games";
+            this.openFileDialogueButton.UseVisualStyleBackColor = true;
+            this.openFileDialogueButton.Click += new System.EventHandler(this.openFileDialogueButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1137, 665);
+            this.Controls.Add(this.openFileDialogueButton);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login to: HowLongToBeat.com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button openFileDialogueButton;
+    }
+}
+
---
+++ HowLongToBeat Game/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HowLongToBeat_Game
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //set original size
+            this.Width = 330;
+            this.Height = 146;
+
+            //Hide webbrowser
+            webBrowser1.Visible = false;
+            openFileDialogueButton.Visible = false;
+
+            string BaseDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory));
+
+            if (!File.Exists($"{BaseDirectory}\\Information\\login.txt"))
+            {
+                
+            } else
+            {
+                StreamReader reader = new StreamReader($"{BaseDirectory}\\Information\\login.txt");
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split('|');
+
+                    username.Text = values[0];
+                    password.Text = values[1];
+
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Hide controls & prevent them from being used again.
+            label1.Hide();
+            label2.Hide();
+            button1.Hide();
+            username.Hide();
+            password.Hide();
+
+            //Resizing the form after button is clicked
+            Form1.ActiveForm.Width = 800;
+            Form1.ActiveForm.Height = 690;
+
+            //Setting location of window according to new size
+            Form1.ActiveForm.Left = Form1.ActiveForm.Width - 100;
+            Form1.ActiveForm.Top = Form1.ActiveForm.Height - 600;
+
+            //bring back the webbrowser
+            webBrowser1.Visible = true;
+
+            //Inputs the values from the textboxes into the form.
+            var InputElements = webBrowser1.Document.GetElementsByTagName("input");
+
+            foreach (HtmlElement i in InputElements)
+            {
+                if (i.GetAttribute("name").Equals("username")) {
+                    i.Focus();
+                    i.InnerText = username.Text;
+                }
+
+                if (i.GetAttribute("name").Equals("password"))
+                {
+                    i.Focus();
+                    i.InnerText = password.Text;
+                }
+                if (i.GetAttribute("name").Equals("Submit"))
+                {
+                    i.InvokeMember("click");
+                }
+            }
+            webBrowser1.Navigate("https://howlongtobeat.com/submit.php");
+
+            openFileDialogueButton.Visible = true;
+            
+        }
+
+        private void openFileDialogueButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Browse Text Files";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Gets the file location from fileDialog
+                var fileName = openFileDialog1.FileName;
+                string line;
+                var lineCount = File.ReadAllLines($"{fileName}").Length;
+                string[] gamelist = new string[lineCount];
+
+                //Writes name to console, for no other reason than debugging.
+                Console.WriteLine(fileName);
+
+                // Read the file and display it line by line.  
+                StreamReader file = new StreamReader($"{fileName}");
+                int counter = 0;
+                while ((line = file.ReadLine()) != null)
+                {
+                    //puts the contents of file into array
+                    gamelist[counter] = line;
+                    counter++;
+                }
+                //method to process array names
+                processGames(gamelist);
+                counter++;
+                file.Close();
+            }
+        }
+
+        //Have issues here in this function.
+        private void processGames(string[] gameList)
+        {
+            for (int count = 0; count < gameList.Length; count++)
+            {
+                //finds all html elements with the tag "input"
+                var InputElements = webBrowser1.Document.GetElementsByTagName("input");
+                foreach (HtmlElement i in InputElements)
+                {
+                    //Same thing again just if the attribute "name" of "input" equals "game_name"
+                    if (i.GetAttribute("name").Equals("game_name"))
+                    {
+                        i.Focus();
+                        //Here's the issue, it just places the whole string at once
+                        //causing it to not be read in the website, for whatever reason
+                        //As a result of this var GamesList = webBrowser1.Document.GetElementsByTagName("h3"); doesn't work properly causing the application
+                        //as a whole to fail at it's job. This needs to be fixed.
+
+                        //Issue: Will copy and paste the name into the textbox on the webpage near instaneously which makes it so that the dropdown
+                        //       doesn't appear causing GamesList to return false.
+                        //Eg;    It'll load https://howlongtobeat.com/submit.php and put the first in your file into it instanenously
+                        //       causes JS not to work, which means that GamesList can't find the <h3> tag it's looking for
+                        //       Which means it doesn't work.
+                        Console.WriteLine(gameList[count]);
+                        i.InnerText = gameList[count];
+
+                        //As a result this is all ignored.
+                        //This needs to be tested when the above bit works as it's supposed to.
+                        var GamesList = webBrowser1.Document.GetElementsByTagName("h3");
+                        foreach (HtmlElement game in GamesList)
+                        {
+                            if (game.InnerText.Equals(gameList[count].ToString()))
+                            {
+                                game.InvokeMember("click");
+
+                                var CheckBoxandSubmit = webBrowser1.Document.GetElementsByTagName("input");
+                                foreach (HtmlElement checkbox in CheckBoxandSubmit)
+                                {
+                                    if (checkbox.GetAttribute("name").Equals("list_b"))
+                                    {
+                                        webBrowser1.Document.GetElementById("list_b").InvokeMember("CLICK");
+                                    }
+                                    if (checkbox.GetAttribute("name").Equals("submitted"))
+                                    {
+                                        checkbox.InvokeMember("click");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
